26084c4243510c882b771de22109cd21ccb8f5e1
D Hellen <88243822+DominickHellen@users.noreply.github.com>
2023-01-12 12:31:59 -0500

    Update CellMatricesQuant.py

--- CellMatricesQuant.py
+++ CellMatricesQuant.py
@@ -1,35 +1,30 @@
-
-from ij.plugin.frame import RoiManager
-
-rm = RoiManager.getInstance()
-
-
 from ij import IJ 
-
+from ij.plugin.frame import RoiManager
+from ij.gui import GenericDialog
 import random
 
 imp = IJ.getImage()
 
+rm = RoiManager.getInstance()
 IJ.run("ROI Manager...", "");
+width = imp.getWidth()
+height = imp.getHeight()
+x = random.randint(1,width)
+y = random.randint(1,height)
 
-x = random.randint(1,2048)
-y = random.randint(400,1200)
-
-roi = imp.setRoi(x,y,400,400);
-
-rm.addRoi(roi);
-
-print imp
-
-imp = imp.resize(400, 400, "bilinear");
-
-IJ.run("Duplicate...", "new");
-
+def ROI():
+	roi = imp.setRoi(x,y,300,300);
+	rm.addRoi(roi);
+	IJ.run("Duplicate...", "new");
+		
+ROI()
 imp = IJ.getImage()
 
-IJ.run(imp, "Find Edges", "");
-IJ.setAutoThreshold(imp, "Default dark");
-IJ.run(imp, "Convert to Mask", "");
-IJ.run(imp, "Outline", "");
+def Cell_Geometrics():
+	IJ.run(imp, "Find Edges", "");
+	IJ.setAutoThreshold(imp, "Default dark");
+	IJ.run(imp, "Convert to Mask", "");
+	IJ.run(imp, "Outline", "");
+	IJ.run("Hough Circle Transform","minRadius=5, maxRadius=30, inc=1, minCircles=1, maxCircles=65535, threshold=0.6, resolution=233, ratio=1.0, bandwidth=10, local_radius=10,  reduce show_mask show_centroids results_table");
 
-IJ.run("Hough Circle Transform","minRadius=5, maxRadius=30, inc=1, minCircles=1, maxCircles=65535, threshold=0.65, resolution=233, ratio=1.0, bandwidth=10, local_radius=10,  reduce show_mask show_centroids results_table");
+Cell_Geometrics()

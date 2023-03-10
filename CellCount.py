85e9998b1e6cc9480b4036feee77df0e02d59088
D Hellen <88243822+DominickHellen@users.noreply.github.com>
2023-03-09 22:31:22 -0500

    Update CellCount.py

--- CellCount.py
+++ CellCount.py
@@ -8,8 +8,8 @@ imp = IJ.getImage()
 def Cell_counter():
 #These two steps below can be skipped and done by hand, depending on the focal plane & brightness of the image
 	IJ.run(imp, "Find Edges", "");
-	IJ.run(imp, "Convert to Mask", "");
 #These steps below need to be run for everything
+	IJ.run(imp, "Convert to Mask", "");
 	IJ.run(imp, "Dilate", "");
 	IJ.run(imp, "Fill Holes", "");
 	IJ.run(imp, "Watershed", "");

9d23ef423fb4048e6e852a50cef366d8005203c8
D Hellen <88243822+DominickHellen@users.noreply.github.com>
2023-03-26 09:19:03 -0400

    Update AutomaticConfluency.py

--- AutomaticConfluency.py
+++ AutomaticConfluency.py
@@ -1,19 +1,30 @@
+# Import necessary modules
 from ij import IJ 
-from ij.plugin.frame import RoiManager
-from ij.gui import GenericDialog
 import random
 
+# Get the active image
 imp = IJ.getImage()
 
-
-
+# Define a function to calculate confluency
 def Confluency():
-	IJ.run(imp, "Find Edges", "");
-	IJ.run(imp, "Manual Threshold...", "min=5141 max=65535");
-	IJ.run(imp, "Convert to Mask", "");
-	IJ.run(imp, "Convert to Mask", "");
-	IJ.run(imp, "Dilate", "");
-	IJ.run(imp, "Fill Holes", "");
-	IJ.run(imp, "Analyze Particles...", "size=200-Infinity pixel circularity=0-1.00 show=Outlines summarize composite");
-	
+    
+    # Find edges in the image to help with segmentation
+    IJ.run(imp, "Find Edges", "");
+    
+    # Manually threshold the image to separate foreground from background
+    IJ.run(imp, "Manual Threshold...", "min=5141 max=65535");
+    
+    # Convert the image to a binary mask
+    IJ.run(imp, "Convert to Mask", "");
+    
+    # Dilate the mask to fill in gaps between cells
+    IJ.run(imp, "Dilate", "");
+    
+    # Fill any holes in the mask
+    IJ.run(imp, "Fill Holes", "");
+    
+    # Analyze particles to calculate confluency
+    IJ.run(imp, "Analyze Particles...", "size=200-Infinity pixel circularity=0-1.00 show=Outlines summarize composite");
+
+# Call the function to calculate confluency
 Confluency()
